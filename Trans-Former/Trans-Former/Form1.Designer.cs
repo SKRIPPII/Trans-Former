cd665cbf8875ddefea1f6f3014f79cbcf868dce4
SKRIPPII <93274805+SKRIPPII@users.noreply.github.com>
2022-04-19 03:49:57 +0300

    Add files via upload

---
+++ Trans-Former/Trans-Former/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Trans_Former
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(537, 70);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(251, 368);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Каталог ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(532, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Корзина";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(297, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(297, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(252, 368);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(297, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 33);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Итого:";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Промывочное масло",
+            "Масло моторное Mobil Super 3000",
+            "Масло моторное Formula F",
+            "Масло моторное синтетика 5W-40",
+            "Масло моторное Engine Oil 5W-30"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Масло";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Масляный фильтр HYUNDAI-KIA",
+            "Масляный фильтр Mann-Filter",
+            "Фильтр воздушный Nitto AN-222V NISSAN",
+            "Фильтр масляный Nitto O-118 TOYOTA",
+            "Фильтр топливный VAG 6Q0 201 051J"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 109);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 14;
+            this.comboBox2.Text = "Фильтра";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Компрессор кондиционера",
+            "Компрессор кондиционера LUZAR LCAC 0578",
+            "Компрессор кондиционера NISSENS 89221",
+            "Компрессор кондиционера BMW E81",
+            "Компрессор кондиционера LUZAR LCAC 14JB"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 152);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 15;
+            this.comboBox3.Text = "КПП";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Сальники КПП ВАЗ 2110",
+            "Муфта синхронизатора КПП ВАЗ 2101-07",
+            "Рычаг переключения КПП, кулиса ВАЗ 2108-09",
+            "Ремкомплект подшипников и прокладок КПП",
+            "Шарнир кулисы КПП ВАЗ 1118 в упаковке"});
+            this.comboBox4.Location = new System.Drawing.Point(12, 192);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 16;
+            this.comboBox4.Text = "Кондиционеры";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(297, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 32);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(297, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Колличество\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FailToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 19;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FailToolStripMenuItem
+            // 
+            this.FailToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SaveToolStripMenuItem,
+            this.OpenToolStripMenuItem});
+            this.FailToolStripMenuItem.Name = "FailToolStripMenuItem";
+            this.FailToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.FailToolStripMenuItem.Text = "Файл";
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SaveToolStripMenuItem.Text = "Сохранить";
+            this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.OpenToolStripMenuItem.Text = "Открыть";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(94, 20);
+            this.toolStripMenuItem1.Text = "О программе";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(12, 20);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Trans_Former.Properties.Resources.makvin;
+            this.pictureBox1.Location = new System.Drawing.Point(297, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ Trans-Former/Trans-Former/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Trans_Former
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        class Transformer
+        {
+            int price;
+            int kol;
+            string name;
+            public Transformer(int kol, int price,string name) { this.kol = kol;  this.price = price;this.name = name; }
+
+
+            public int Price
+            {
+                get {return price;  }
+                set { price = value; }
+            }
+
+            public int Kol
+            {
+                get { return kol; }
+                set { kol = value; }
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+        List<Transformer> katalog = new List<Transformer>();
+        List<int> cena = new List<int>();
+        public Form1()
+            {
+
+
+                InitializeComponent();
+
+            for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                cena.Add((i + 5) * 50);
+               
+            }
+            for (int i = 0; i < comboBox2.Items.Count; i++)
+            {
+                cena.Add((i + 5) * 70);
+            }
+
+            for (int i = 0; i < comboBox3.Items.Count; i++)
+            {
+                cena.Add((i + 5) * 90);
+            }
+
+
+            for (int i = 0; i < comboBox4.Items.Count; i++)
+            {
+                cena.Add((i + 5) * 120);
+            }
+        }
+
+            private void Form1_Load(object sender, EventArgs e) // картинка при загрузке формы
+            {
+                Form2 form = new Form2();
+                form.Show();
+                form.Update();
+                System.Threading.Thread.Sleep(5000);
+                form.Close();
+            
+             }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kol;
+            bool ifkol = int.TryParse(textBox1.Text.Trim(), out kol);
+            if(ifkol && comboBox1.SelectedIndex > -1 && textBox1.Text.Length > 0)
+            {
+                Transformer transformer = new Transformer(kol,cena[comboBox1.SelectedIndex] * kol,comboBox1.SelectedItem.ToString());
+                katalog.Add(transformer);
+                Print(katalog);
+                comboBox1.SelectedIndex = -1;
+
+            }
+
+            if (ifkol && comboBox2.SelectedIndex > -1 && textBox1.Text.Length > 0)
+            {
+                Transformer transformer = new Transformer(kol, cena[comboBox2.SelectedIndex] * kol, comboBox2.SelectedItem.ToString());
+                katalog.Add(transformer);
+                Print(katalog);
+                comboBox2.SelectedIndex = -1;
+
+            }
+
+
+            if (ifkol && comboBox3.SelectedIndex > -1 && textBox1.Text.Length > 0)
+            {
+                Transformer transformer = new Transformer(kol, cena[comboBox3.SelectedIndex]*kol, comboBox3.SelectedItem.ToString());
+                katalog.Add(transformer);
+                Print(katalog);
+                comboBox3.SelectedIndex = -1;
+
+            }
+
+            if (ifkol && comboBox4.SelectedIndex > -1 && textBox1.Text.Length > 0)
+            {
+                Transformer transformer = new Transformer(kol, cena[comboBox4.SelectedIndex]*kol, comboBox4.SelectedItem.ToString());
+                katalog.Add(transformer);
+                Print(katalog);
+                comboBox4.SelectedIndex = -1;
+
+            }
+        }
+
+
+        void Print(List<Transformer> print)
+        {
+            listBox2.Items.Clear();
+           foreach(var i in print)
+            {
+                listBox2.Items.Add(i.Name + " " +  i.Kol + " шт " + i.Price * i.Kol + " руб");
+            }
+        }
+
+
+     
+
+        void Sum(List<Transformer> katsum)//посчитать сумму
+        {
+            int sum = 0;
+            foreach (Transformer item in katsum)
+            {
+                sum += item.Kol * item.Price;
+            }
+            MessageBox.Show("Итого: " + sum.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e) // удалить
+        {
+            if(listBox2.SelectedIndex > -1)
+            {
+                katalog.RemoveAt(listBox2.SelectedIndex);
+                Print(katalog);
+                Sum(katalog);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Sum(katalog);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Form3 form = new Form3();
+            form.ShowDialog();
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e) // Сохранить
+        {
+            const string sPath = "transformer.txt";
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in listBox2.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+            MessageBox.Show("Programs saved!");
+        }
+
+        
+    }
+
+
+    }
+
